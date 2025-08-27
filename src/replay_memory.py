6a151e1a85507d79eed9f55cbcd7ea8d18fa9878
Samed Dallaa <samadodallaa@gmail.com>
2025-08-28 00:29:59 +0100

    feat: added replay memory

---
+++ src/replay_memory.py
@@ -0,0 +1,16 @@
+from collections import deque
+import random
+
+class ReplayMemory:
+    def __init__(self, capacity):
+        self.capacity = capacity
+        self.memory = deque(maxlen=capacity)
+
+    def append(self, transition):
+        self.memory.append(transition)
+
+    def sample(self, batch_size):
+        return random.sample(self.memory, batch_size)
+
+    def __len__(self):
+        return len(self.memory)
